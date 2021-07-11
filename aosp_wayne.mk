93d00044346a1805e18f23bb162ae344761cae42
Christian de la Cruz <chrisdlc119@outlook.com>
2021-07-10 22:59:04 -0500

    wayne: Nuke PPUI props

--- aosp_wayne.mk
+++ aosp_wayne.mk
@@ -33,13 +33,6 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.ppui.device_name=wayne \
-	ro.ppui.version=3.6 \
-	ro.ppui.version_code=Unicorn \
-	ro.ppui.is_official=true \
-	ro.ppui.maintainer_name=chdelacr
-
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
